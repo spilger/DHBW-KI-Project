5d6bacc1d505933f631a188b68aad74e276b987d
Anna-Lena Blinken <Anna-lena.blinken@sap.com>
2021-05-25 23:07:38 +0200

    feat: add comments

--- image classifier/doggo.py
+++ image classifier/doggo.py
@@ -82,7 +82,7 @@ first_image = image_batch[0]
 # Notice the pixels values are now in `[0,1]`.
 print(np.min(first_image), np.max(first_image))
 
-# Überanpassung loswerden
+# Verhinderung einer Überanpassung durch Rotieren und Zoomen der Bilder  
 data_augmentation = tf.keras.Sequential(
   [
     layers.experimental.preprocessing.RandomFlip("horizontal", 
